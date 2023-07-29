0159a4df5bb6ea0c77c315de41bd074e7646fc99
Nelson Antunes <neryuuk@neryuuk.com>
2023-07-29 14:51:31 -0300

    testing externally

--- tw_dicio.py
+++ tw_dicio.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from lxml import html
-from urllib import request
+# pylint: disable=unused-argument, wrong-import-position
 
-base_url = "https://www.dicio.com.br"
-pagina = request.urlopen(base_url)
+from dicio import *
 
-tree = html.fromstring(str(pagina.read()))
-doDia = tree.xpath("//*[@class='word-link']")
-url = base_url + doDia[0].attrib["href"]
-definir = "Palavra do dia: {}\n{}".format(doDia[0].text, url)
 
-print(definir)
+# print(buscarPalavraDoDia())
+print(buscarDefinicao("ornitorrinco"))
