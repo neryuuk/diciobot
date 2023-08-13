da404a438d46f2927876a04e262c15f5b214ad74
Nelson Antunes <neryuuk@neryuuk.com>
2023-08-13 16:36:06 -0300

    recursive iteration through error message

--- telegram_bot.py
+++ telegram_bot.py
@@ -261,27 +261,24 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
         # exc_info=context.error
     )
 
-    tb_list = format_exception(
-        None,
-        context.error,
-        context.error.__traceback__
-    )
-    tb_string = "".join(tb_list)
-
     update_str = update.to_dict() if isinstance(update, Update) else str(update)
-    message = (
-        f"An exception was raised while handling an update\n"
-        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}</pre>\n\n"
-        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
-        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
-        f"<pre>{html.escape(tb_string)}</pre>"
+    await iterateError(
+        html.escape(json.dumps(update_str, indent=2, ensure_ascii=False)),
+        context
     )
 
-    await context.bot.send_message(
-        chat_id=CHAT_ID,
-        text=message,
-        parse_mode=ParseMode.HTML,
-    )
+    tb_str = "".join(format_exception(
+        None, context.error, context.error.__traceback__
+    ))
+    await iterateError(html.escape(tb_str), context)
+
+
+async def iterateError(content: str, context: ContextTypes.DEFAULT_TYPE, start: int = 0):
+    MAX_LEN = 4000
+
+    await context.bot.send_message(CHAT_ID, content[start:start + MAX_LEN], ParseMode.HTML)
+    if len(content[start:]) > MAX_LEN:
+        await iterateError(content, context, start + MAX_LEN)
 
 
 def main() -> None:
