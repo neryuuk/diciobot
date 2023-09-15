c627318f29da85290ce7fd956fb75d734cf05a4a
Nelson Antunes <966712+neryuuk@users.noreply.github.com>
2023-09-15 20:17:54 -0300

    get word method

--- source/redis_manager.py
+++ source/redis_manager.py
@@ -1,10 +1,26 @@
-import redis
 from dotenv import load_dotenv
 from os import getenv
+from redis import ConnectionPool, Redis
+from redis.commands.json.path import Path
+
 load_dotenv()
 
-conn = redis.Redis(
+pool = ConnectionPool(
     host=getenv('REDIS_HOST'),
     port=getenv('REDIS_PORT'),
+    username=getenv('REDIS_USER'),
     password=getenv('REDIS_PASS'),
+    db=0,
 )
+conn = Redis(connection_pool=pool)
+
+
+def get(word: str):
+    if not word:
+        return None
+
+    if len(word) == 0:
+        return None
+
+    content = conn.json().get(word)
+    return content
